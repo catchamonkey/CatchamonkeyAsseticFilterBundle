3be34734ba638fed7b3091a159c0f7f7844fab92
Chris Sedlmayr <chris@sedlmayr.co.uk>
2012-11-25 21:48:38 +0000

    Adds DI Configuration class

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the CatchamonkeyAsseticFilterBundle package.
+ *
+ * (c) catchamonkey <http://github.com/catchamonkey>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Catchamonkey\Bundle\AsseticFilterBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Handles configuration information for the bundle 
+ *
+ * @author Chris Sedlmayr (catchamonkey) <chris@sedlmayr.co.uk>
+ */
+class Configuration implements ConfigurationInterface
+{
+}
