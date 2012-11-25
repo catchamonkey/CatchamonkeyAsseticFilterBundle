d2cb6ebb4f84add8ff7f1b45da37f745dd2e523f
Chris Sedlmayr <chris@sedlmayr.co.uk>
2012-11-25 21:52:18 +0000

    Adds DI Extension class, loads services

---
+++ DependencyInjection/CatchamonkeyAsseticFilterExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the CatchamonkeyAsseticFilterBundle package.
+ *
+ * (c) catchamonkey <http://github.com/catchamonkey>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Catchamonkey\Bundle\AsseticFilterBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+class CatchamonkeyAsseticFilterExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+
+    public function getAlias()
+    {
+        return 'catchamonkey_cssmin';
+    }
+}
