ea59c6805a0b69e09b606976faa4b84cf191b910
Chris Sedlmayr <chris@sedlmayr.co.uk>
2012-11-27 09:16:50 +0000

    * Adds Bundle class * adds more keywords for composer

---
+++ CatchamonkeyAsseticFilterBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the CatchamonkeyAsseticFilterBundle package.
+ *
+ * (c) catchamonkey <http://github.com/catchamonkey>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Catchamonkey\Bundle\AsseticFilterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CatchamonkeyAsseticFilterBundle extends Bundle
+{
+}
